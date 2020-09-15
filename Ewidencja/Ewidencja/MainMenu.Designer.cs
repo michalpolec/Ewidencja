df8337a37f53369b6c8117ae25dbdc9c06e86c08
Michał Połeć <michal.polec1@gmail.com>
2020-09-15 12:23:48 +0200

    DESIGN

--- Ewidencja/Ewidencja/MainMenu.Designer.cs
+++ Ewidencja/Ewidencja/MainMenu.Designer.cs
@@ -38,7 +38,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(24, 32);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(247, 39);
+            this.button1.Size = new System.Drawing.Size(334, 39);
             this.button1.TabIndex = 0;
             this.button1.Text = "Ewidencja maszynistów";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,7 +48,7 @@
             // 
             this.button2.Location = new System.Drawing.Point(24, 96);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(247, 39);
+            this.button2.Size = new System.Drawing.Size(334, 39);
             this.button2.TabIndex = 1;
             this.button2.Text = "Ewidencja kierowników";
             this.button2.UseVisualStyleBackColor = true;
@@ -59,7 +59,7 @@
             this.pictureBox1.Image = global::Ewidencja.Properties.Resources.logo;
             this.pictureBox1.Location = new System.Drawing.Point(24, 161);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(247, 174);
+            this.pictureBox1.Size = new System.Drawing.Size(334, 174);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -68,7 +68,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(297, 347);
+            this.ClientSize = new System.Drawing.Size(381, 347);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
